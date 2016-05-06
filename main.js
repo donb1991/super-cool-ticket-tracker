e6308530174238d651e93d65c10d43e83c65b0f3
donb1991 <donb1991@gmail.com>
2016-05-06 10:46:28 -0700

    added ; to end of lines

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-const electron = require('electron')
+const electron = require('electron');
 
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 
-let mainWindow
+let mainWindow;
 
 function createWindow () {
 
-  const screen = require('screen')
+  const screen = require('screen');
   let windowSize = screen.getPrimaryDisplay().workAreaSize;
-  mainWindow = new BrowserWindow({width: 450, height: windowSize.height})
+  mainWindow = new BrowserWindow({width: 450, height: windowSize.height});
   mainWindow.setPosition(windowSize.width - 450, 0);
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
