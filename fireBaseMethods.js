e9ff552cc834a8227d2dd1fd0e7dc5bbc8833d4a
donb1991 <donb1991@gmail.com>
2016-05-09 10:30:33 -0700

    refactored updatetimesegment

--- fireBaseMethods.js
+++ fireBaseMethods.js
@@ -39,22 +39,23 @@ function fireBaseMethods() {
 
     updateTimeSegment: function(ticketNumber, timeSegment) {
       let user = this.currentUser();
+      let ticketRef = new Firebase("https://sizzling-heat-8454.firebaseio.com/users/" + user + ticketNumber);
       if(!user) {
         throw("Must be logged in to make changes to a ticket");
       }
-      ref.child("users").child(user).child(ticketNumber).orderByKey().once('value', function(snapshot) {
+      ticketRef.orderByKey().once('value', function(snapshot) {
         let currentTimeSegments = snapshot.val();
         if(currentTimeSegments.timeSegments === undefined) {
-          ref.child("users").child(user).child(ticketNumber).child('timeSegments').push(timeSegment);
+          ticketRef.child('timeSegments').push(timeSegment);
         } else {
           let keys = Object.keys(currentTimeSegments.timeSegments);
           for(var i = 0; i < keys.length; i++) {
             if(currentTimeSegments.timeSegments[keys[i]].end === undefined) {
-              ref.child("users").child(user).child(ticketNumber).child('timeSegments').child(keys[i]).update(timeSegment);
+              ticketRef.child('timeSegments').child(keys[i]).update(timeSegment);
               return;
             }
           }
-          ref.child("users").child(user).child(ticketNumber).child('timeSegments').push(timeSegment);
+          ticketRef.child('timeSegments').push(timeSegment);
         }
       });
     }
