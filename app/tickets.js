6fb47e07b6baa812f5ccfa2894ea82de3d50282c
Donald Brunais <donb1991@hotmail.com>
2016-06-04 14:46:50 -0700

    tickets now pull tickets from firebase

--- app/tickets.js
+++ app/tickets.js
@@ -9,6 +9,25 @@ let Tickets = React.createClass({
     if(!currentUser) {
       // document.location.hash = '#/';
     }
+    fireBaseMethods.getUserTickets(currentUser).then((data) => {
+      let newState = {};
+      newState.inProduction = [];
+      newState.waitingForCMFeedback = [];
+      newState.inQA = [];
+      newState.closed = [];
+      for(let ticket in data) {
+        if(data[ticket].closed) {
+          newState.closed.push(data[ticket]);
+        } else if(data[ticket].ticketTask == "task3"){
+          newState.inQA.push(data[ticket]);
+        } else if(data[ticket].cmFeedback) {
+          newState.waitingForCMFeedback.push(data[ticket]);
+        } else {
+          newState.inProduction.push(data[ticket]);
+        }
+      }
+      this.setState({tickets: newState});
+    });
   },
 
   handleClick(e) {
@@ -16,6 +35,38 @@ let Tickets = React.createClass({
   },
 
   render() {
+    let production = "";
+    let cmFeedback = "";
+    let inQA = "";
+    let closed = "";
+    let previewLinkSent = "";
+    let activated = "";
+    if(this.state) {
+      production = this.state.tickets.inProduction.map((ticket, index) => {
+        return <li key={index}>
+          <p>{ticket.ticketNumber}</p>
+          <p>&emsp;{ticket.ticketTitle}</p>
+        </li>
+      });
+      cmFeedback = this.state.tickets.waitingForCMFeedback.map((ticket, index) => {
+        return <li key={index}>
+          <p>{ticket.ticketNumber}</p>
+          <p>&emsp;{ticket.ticketTitle}</p>
+        </li>
+      });
+      inQA = this.state.tickets.inQA.map((ticket, index) => {
+        return <li key={index}>
+          <p>{ticket.ticketNumber}</p>
+          <p>&emsp;{ticket.ticketTitle}</p>
+        </li>
+      });
+      closed = this.state.tickets.closed.map((ticket, index) => {
+        return <li key={index}>
+          <p>{ticket.ticketNumber}</p>
+          <p>&emsp;{ticket.ticketTitle}</p>
+        </li>
+      });
+    }
     return (
       <div>
         <div className="form-group">
@@ -29,53 +80,33 @@ let Tickets = React.createClass({
         <h4>Current Ticket</h4>
         <ul>
           <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
+              <p>RITM3528330</p>
+              <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
           </li>
         </ul>
         <hr />
 
         <h4>Production</h4>
         <ul>
-          <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
-          </li>
-          <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
-          </li>
+          {production}
         </ul>
         <hr />
 
         <h4>Waiting on CM feedback</h4>
         <ul>
-          <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
-          </li>
-          <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
-          </li>
+          {cmFeedback}
         </ul>
         <hr />
 
         <h4>In QA</h4>
         <ul>
-          <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
-          </li>
+          {inQA}
         </ul>
         <hr />
 
         <h4>Closed</h4>
         <ul>
-          <li>
-            <p>RITM3528330</p>
-            <p>&emsp;Launch: Broadwell-E > Core i7-6950x animation</p>
-          </li>
+          {closed}
         </ul>
         <hr />
       </div>
